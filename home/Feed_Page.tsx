33ce49809138430fcd4c3d5c70a6eb3bebb8b4e9
Willroy Myles <willroymyles@yahoo.com>
2020-07-27 01:14:39 -0500

    Added leaderboard page

--- home/Feed_Page.tsx
+++ home/Feed_Page.tsx
@@ -3,6 +3,7 @@ import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs
 import {View, Colors} from "react-native-ui-lib"
 import uiManager from "../dataLayer/UiManager"
 import MemoriesView from "./views/MemoriesView"
+import LeaderboardView from "./views/LeaderboardView"
 
 const tab = createMaterialTopTabNavigator()
 
@@ -24,7 +25,7 @@ const Feed_Page = () => {
 					inactiveTintColor: uiManager.theme.teritairy_text,
 				}}>
 				<tab.Screen name="memories" component={MemoriesView} />
-				{/* <tab.Screen name="leaderBoard" component={SettingsAbout} /> */}
+				<tab.Screen name="leaderBoard" component={LeaderboardView} />
 			</tab.Navigator>
 		</View>
 	)
---
+++ home/views/LeaderboardView.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import {View, Text} from "react-native-ui-lib"
+
+const LeaderboardView = () => {
+	return (
+		<View>
+			<Text>leader board</Text>
+		</View>
+	)
+}
+
+export default LeaderboardView
