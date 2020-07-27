f6bb2c3ead2a624e03eb5941ee0c58014e474996
Willroy Myles <willroymyles@yahoo.com>
2020-07-27 01:12:30 -0500

    Created memories virw

--- home/Feed_Page.tsx
+++ home/Feed_Page.tsx
@@ -1,66 +1,31 @@
-import React, {useEffect, useState} from "react"
-import {View, Button, Text} from "react-native-ui-lib"
-import dataProvider from "../dataLayer/DataStore"
-import {FlatList} from "react-native"
-import {useNavigation} from "@react-navigation/native"
-import {FeedItemModel} from "../universial/Models"
-import {useTheme} from "styled-components"
-import Feed_itemV2 from "../components/Feed_itemV2"
-import {values} from "mobx"
-import moment from "moment"
-import Feed_Item from "../components/Feed_Item"
+import React from "react"
+import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs"
+import {View, Colors} from "react-native-ui-lib"
+import uiManager from "../dataLayer/UiManager"
+import MemoriesView from "./views/MemoriesView"
+
+const tab = createMaterialTopTabNavigator()
 
 const Feed_Page = () => {
-	const theme = useTheme()
-	const navigation = useNavigation()
-	const [data, setdata] = useState<FeedItemModel[]>([])
-	useEffect(() => {
-		getPastEvents()
-	}, [])
-
-	const handleViewClick = (item: FeedItemModel) => {
-		dataProvider.currentEvent = item
-		navigation.navigate("event", item)
-	}
-
-	const getPastEvents = () => {
-		//should really fetch data and sort in data layer...
-		const d: FeedItemModel[] = []
-		dataProvider.data.forEach((val: FeedItemModel, key) => {
-			if (checkDate(val.date)) d.push(val)
-		})
-
-		setdata(d)
-	}
-
-	const checkDate = (d: string) => {
-		const old = Date.parse(d)
-		const comp = new Date().valueOf()
-		// console.log(d, d < comp)
-
-		return old <= comp
-	}
-
 	return (
-		<View bg-background flex>
-			<FlatList
-				onScroll={() => {}}
-				// style={{borderWidth: 0, flex: 1}}
-				data={data}
-				renderItem={({item, index}) => {
-					return (
-						<View key={index}>
-							<Feed_Item onClick={handleViewClick} index={index} item={item} />
-						</View>
-					)
+		<View flex>
+			<tab.Navigator
+				sceneContainerStyle={{
+					backgroundColor: uiManager.theme.background,
+					flex: 1,
+					padding: 10,
+					// flex: 1,
+					// borderWidth: 10,
+					// minHeight: "100%",
 				}}
-				keyExtractor={(item: FeedItemModel) => item.reference || fakerStatic.random.number(20000000000).toString()}
-			/>
-			<View center>
-				<Button onPress={() => getPastEvents()}>
-					<Text btn>load feed </Text>
-				</Button>
-			</View>
+				tabBarOptions={{
+					tabStyle: {backgroundColor: uiManager.theme.background, borderWidth: 0, elevation: 10},
+					activeTintColor: Colors.primary,
+					inactiveTintColor: uiManager.theme.teritairy_text,
+				}}>
+				<tab.Screen name="memories" component={MemoriesView} />
+				{/* <tab.Screen name="leaderBoard" component={SettingsAbout} /> */}
+			</tab.Navigator>
 		</View>
 	)
 }
---
+++ home/views/MemoriesView.tsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from "react"
+import {View, Button, Text} from "react-native-ui-lib"
+import {FlatList} from "react-native"
+import {useNavigation} from "@react-navigation/native"
+import {useTheme} from "styled-components"
+import {values} from "mobx"
+import moment from "moment"
+import {FeedItemModel} from "../../universial/Models"
+import dataProvider from "../../dataLayer/DataStore"
+import Feed_Item from "../../components/Feed_Item"
+const MemoriesView = () => {
+	const theme = useTheme()
+	const navigation = useNavigation()
+	const [data, setdata] = useState<FeedItemModel[]>([])
+	useEffect(() => {
+		getPastEvents()
+	}, [])
+
+	const handleViewClick = (item: FeedItemModel) => {
+		dataProvider.currentEvent = item
+		navigation.navigate("event", item)
+	}
+
+	const getPastEvents = () => {
+		//should really fetch data and sort in data layer...
+		const d: FeedItemModel[] = []
+		dataProvider.data.forEach((val: FeedItemModel, key) => {
+			if (checkDate(val.date || "")) d.push(val)
+		})
+
+		setdata(d)
+	}
+
+	const checkDate = (d: string) => {
+		const old = Date.parse(d)
+		const comp = new Date().valueOf()
+		// console.log(d, d < comp)
+
+		return old <= comp
+	}
+
+	return (
+		<View bg-background flex>
+			<FlatList
+				onScroll={() => {}}
+				// style={{borderWidth: 0, flex: 1}}
+				data={data}
+				renderItem={({item, index}) => {
+					return (
+						<View key={index}>
+							<Feed_Item onClick={handleViewClick} index={index} item={item} />
+						</View>
+					)
+				}}
+				keyExtractor={(item: FeedItemModel) => item.reference || fakerStatic.random.number(20000000000).toString()}
+			/>
+			<View center>
+				<Button onPress={() => getPastEvents()}>
+					<Text btn>load feed </Text>
+				</Button>
+			</View>
+		</View>
+	)
+}
+
+export default MemoriesView
