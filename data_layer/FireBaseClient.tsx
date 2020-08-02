6c60b831e7634d75dd10045bfdc9310647ae14a5
Willroy Myles <willroymyles@yahoo.com>
2020-08-02 14:32:21 -0500

    Added log out function

--- data_layer/FireBaseClient.tsx
+++ data_layer/FireBaseClient.tsx
@@ -1,31 +1,36 @@
-
-import auth from '@react-native-firebase/auth'
+import auth from '@react-native-firebase/auth';
 class Store {
+  isLoggedIn = () => auth().currentUser == undefined;
 
-    isLoggedIn = () => auth().currentUser == undefined
+  login = (email: string, password: string) =>
+    new Promise((resolve, reject) => {
+      auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((res) => {
+          resolve(res.user.uid);
+        })
+        .catch((err) => reject(err));
+    });
 
-    login = (email: string, password: string) => new Promise((resolve, reject) => {
+  register = (username: string, email: string, password: string) =>
+    new Promise((resolve, reject) => {
+      auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((res) => {
+          auth()
+            .currentUser?.updateProfile({
+              displayName: username,
+            })
+            .then((eh) => {
+              resolve(res.user.uid);
+            })
+            .catch((err) => reject(err));
+        })
+        .catch((err) => reject(err));
+    });
 
-        auth().signInWithEmailAndPassword(email, password).then(res => {
-            resolve(res.user.uid)
-        }).catch(err => reject(err))
-    })
-
-    register = (username: string, email: string, password: string) => new Promise((resolve, reject) => {
-        auth().createUserWithEmailAndPassword(email, password).then(res => {
-            auth().currentUser?.updateProfile({
-                displayName: username,
-
-            }).then(eh => {
-
-                resolve(res.user.uid)
-            }).catch(err => reject(err))
-        }).catch(err => reject(err))
-    })
-
-
-
+  logout = () => auth().signOut();
 }
 
-const FBS = new Store()
-export default FBS
+const FBS = new Store();
+export default FBS;
