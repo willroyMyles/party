2d9391d908d124a8f897191fa9c4c4b3ae959c77
Willroy Myles <willroymyles@yahoo.com>
2020-08-15 00:05:55 -0500

    Fixed warning message from bad date format

--- components/Feed_Item.tsx
+++ components/Feed_Item.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react"
-import {View, Text, TouchableOpacity, Image, Colors} from "react-native-ui-lib"
+import React, { useState, useEffect } from "react"
+import { View, Text, TouchableOpacity, Image, Colors } from "react-native-ui-lib"
 
-import {useNavigation} from "@react-navigation/native"
+import { useNavigation } from "@react-navigation/native"
 import { FeedItemModel } from "../universal/Models"
 import { useTheme } from "styled-components"
 import moment from "moment"
@@ -9,18 +9,32 @@ import BackDrop from "./BackDrop"
 import FireStore from "../data_layer/FireStore"
 import { Dimensions } from "react-native"
 // import * as faker from "faker"
-const { width, height } = Dimensions.get("screen")
+const { width, height } = Dimensions.get( "screen" )
 
-const Feed_Item = ({reference}: {reference :string} ) =>
+const Feed_Item = ( { reference }: { reference: string } ) =>
 {
 	const item = FireStore.data.get( reference )
 	if ( !item ) return <View />
-	
+
 	const theme = useTheme()
 	const navigation = useNavigation()
+	const [image, setimage] = useState<string>()
 
-	const handleClick = () => {
-		navigation.navigate("view event", {reference: item?.reference})
+	useEffect( () =>
+	{
+		async function getImage()
+		{
+			const d = await FireStore.retrieve.imageFromReference( item.reference )
+			setimage( d )
+		}
+
+		getImage()
+
+	}, [] )
+
+	const handleClick = () =>
+	{
+		navigation.navigate( "view event", { reference: item?.reference } )
 	}
 
 	return (
@@ -29,7 +43,7 @@ const Feed_Item = ({reference}: {reference :string} ) =>
 				borderTopWidth: 0,
 				borderBottomWidth: 0,
 				paddingVertical: 5,
-				paddingHorizontal:10,
+				paddingHorizontal: 10,
 				marginVertical: 10,
 				width,
 			}}>
@@ -38,40 +52,40 @@ const Feed_Item = ({reference}: {reference :string} ) =>
 				activeOpacity={0.85}
 				padding-9
 				marginV-8
-			bg-foreground
+				bg-foreground
 
-				style={{ borderWidth: 0, borderRadius: 3, elevation: 0, borderColor: Colors.grey50 , overflow:"hidden"}}>
+				style={{ borderWidth: 0, borderRadius: 3, elevation: 0, borderColor: Colors.grey50, overflow: "hidden" }}>
 				{/* <BackDrop /> */}
 				<View>
-					<View style={{elevation:5}}  center>
+					<View style={{ elevation: 5 }} center>
 						<Image
-							source={{ uri: item.imageUrl }}
-							
-							style={{flex: 1, flexDirection: "row", borderRadius: 3, height: 150}}
+							source={{ uri: image }}
+
+							style={{ flex: 1, flexDirection: "row", borderRadius: 3, height: 150 }}
 							resizeMode="cover"
-							// aspectRatio={height/width * 1.2}
+						// aspectRatio={height/width * 1.2}
 						/>
-						</View>
+					</View>
 					<View
 						row
 						padding-2
 						paddingT-3
 						marginV-15
-						style={{flex: 2, flexDirection: "column", justifyContent: "space-between"}}>
-							<Text lvl1>{item.title}</Text>
+						style={{ flex: 2, flexDirection: "column", justifyContent: "space-between" }}>
+						<Text lvl1>{item.title}</Text>
 						<View row spread >
 							<View>
 								<View marginV-3>
 									<Text muted>Date</Text>
-								{/* <Text regular>{moment(item.date).format("ddd MMM DD, YYYY")}</Text> */}
+									<Text regular>{moment( new Date( item.date || "" ) ).format( "ddd MMM DD, YYYY" )}</Text>
 								</View>
 								<View marginV-3>
 									<Text muted>Starts at</Text>
 									<View row>
-									<Text regular>{moment( item.start ).format( "h:mm a" )}</Text>
-									<Text regular color={Colors.muted}> for </Text>
-									<Text regular>{item.duration}</Text>
-									<Text regular color={Colors.muted}> hrs</Text>
+										<Text regular>{moment( new Date( item.start || "" ) ).format( "h:mm a" )}</Text>
+										<Text regular color={Colors.muted}> for </Text>
+										<Text regular>{item.duration}</Text>
+										<Text regular color={Colors.muted}> hrs</Text>
 									</View>
 								</View>
 							</View>
