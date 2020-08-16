d0ed7653fb6416f7b0a40115bd49d6258bce0eb9
Willroy Myles <willroymyles@yahoo.com>
2020-08-16 15:41:56 -0500

    implemented location tracking

--- pages/NearMe.tsx
+++ pages/NearMe.tsx
@@ -1,23 +1,50 @@
 import React, {useEffect, useRef, useState} from 'react';
-import MapView, {PROVIDER_GOOGLE, Region} from 'react-native-maps';
+import MapView, {
+  Circle,
+  EventUserLocation,
+  PROVIDER_GOOGLE,
+  Region,
+} from 'react-native-maps';
 import {View, Text, Colors, LoaderScreen} from 'react-native-ui-lib';
 import {useTheme} from 'styled-components';
 import {getLocation, getRegion} from '../universal/GetLocation';
+
+const radius = 600;
+const taskName = 'geoLocation';
 
 const NearMe = () => {
   const map = useRef<MapView | undefined>();
   const theme = useTheme();
   const [region, setRegion] = useState<Region | null>();
+  const [userCircle, setUserCircle] = useState<Region | undefined>(undefined);
   useEffect(() => {
     getUserRegion();
     return () => {};
   }, []);
 
+  useEffect(() => {
+    if (region) setUserCircle(region);
+  }, [region]);
+
+  const userLocationChanged = async (e: EventUserLocation) => {
+    try {
+      const loc = e.nativeEvent.coordinate;
+      const reg = await getRegion(loc);
+      setUserCircle(reg);
+      console.log('should be animating');
+    } catch (err) {
+      console.log('err', err);
+    }
+  };
+
   const getUserRegion = async () => {
     try {
       const coor = await getLocation();
       const reg = await getRegion(coor);
-      if (map.current) map.current.animateToRegion(reg);
+      if (map.current) {
+        map.current.animateToRegion(reg);
+        map.current.forceUpdate();
+      }
       setRegion(reg);
       console.log('should be animating');
     } catch (err) {
@@ -31,8 +58,17 @@ const NearMe = () => {
         <MapView
           ref={map}
           showsUserLocation
+          onUserLocationChange={userLocationChanged}
           style={{width: '100%', height: '100%'}}
-          provider={PROVIDER_GOOGLE}></MapView>
+          provider={PROVIDER_GOOGLE}>
+          <Circle
+            radius={radius}
+            center={userCircle}
+            fillColor={Colors.primary + '22'}
+            strokeWidth={1}
+            strokeColor={Colors.grey50}
+          />
+        </MapView>
       </View>
     );
 
