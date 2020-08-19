64c17b355820e53b35cef6bd0d1139e5ec4469c3
Willroy Myles <willroymyles@yahoo.com>
2020-08-19 12:45:06 -0500

    Configured search view

--- components/SearchComponent.tsx
+++ components/SearchComponent.tsx
@@ -6,52 +6,82 @@ import { FeedItemModel } from '../universal/Models'
 import FireStore from '../data_layer/FireStore'
 import FeedItemMemoryVersionOne from './FeedItemMemoryVersionOne copy'
 import Feed_Item from './Feed_Item'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import SearchBar from './SearchBar'
+import { StyleSheet } from 'react-native'
+import { GS } from '../universal/GS'
+
 
 const SearchComponent = () =>
 {
     const [value, setValue] = useState<string>( "" )
-    const [data, setdata] = useState<FeedItemModel[] |undefined>(undefined)
-    
+    const [data, setdata] = useState<FeedItemModel[] | undefined>( undefined )
+
     const onBlur = () =>
     {
-    
+
     }
- 
-    
-    const onChange = ( v : string ) =>
+
+
+    const onChange = ( v: string ) =>
     {
         setValue( v )
-         if ( v.length == 0 )
+        if ( v.length == 0 )
         {
             setdata( undefined )
             return
         }
         const r = new RegExp( `${ v.toLowerCase() }` )
-        console.log(v, r);
-        
-        const d = [...FireStore.data.values()].filter( ( value, index ) => value.title?.toLowerCase().match(r))
-        console.log(d.length);
-        
-        setdata(d)
+        console.log( v, r );
+
+        const d = [...FireStore.data.values()].filter( ( value, index ) => value.title?.toLowerCase().match( r ) )
+        console.log( d.length );
+
+        setdata( d )
     }
 
     return (
-         <View bg-background flex style={{borderWidth:0, justifyContent:"center"}}>
-                <View marginH-10 marginT-15 marginB-0 padding-2>
-                <TextField placeholder="search" onBlur={onBlur} onChangeText={onChange} value={value}
+        <View bg-background flex style={{ borderWidth: 0, justifyContent: "center" }}>
+
+            <View style={s.bar} marginT-10>
+                <TextField placeholderStyle={GS.floater} hideUnderline style={[s.bar, { backgroundColor: Colors.foreground }]} placeholder="search" onBlur={onBlur} onChangeText={onChange} value={value}
                 />
+                <View style={{
+                    position: "absolute",
+                    right: 25,
+                    top: 15
+                }}>
+                    <Icon name="search" size={14} />
+
+                </View>
             </View>
-            <View  flex style={{margin:1, borderColor:Colors.grey50, padding:0}}>
-                <ScrollView>
+            <View flex style={{ margin: 1, borderColor: Colors.grey50, padding: 0 }}>
+                <ScrollView contentContainerStyle={{ minHeight: "100%" }}>
                     {data?.map( ( value, index ) =>
                     {
-                    return <Feed_Item item={value} />
-                })}
+                        return <Feed_Item item={value} />
+                    } )}
+
+                    {value === "" && <View flex-2 center >
+                        <Text lvl2 center>Start typing to begin search</Text>
+                    </View>}
+
+                    {value !== "" && data?.length === 0 && <View flex-10 center >
+                        <Text>No results...</Text>
+                    </View>}
                 </ScrollView>
-              
+
+
             </View>
-            </View>
+        </View>
     )
 }
 
+const s = StyleSheet.create( {
+    bar: {
+        padding: 5,
+        paddingHorizontal: 12,
+        borderRadius: 50
+    }
+} )
 export default SearchComponent
