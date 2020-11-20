3fd6bb1a5f78476cb4bfc6b0a90bd9ad8b81073e
W <w@Ws-MacBook-Pro.local>
2020-11-20 16:10:19 -0500

    Completed blur view implementation

--- components/EventHeaderImage.tsx
+++ components/EventHeaderImage.tsx
@@ -8,14 +8,15 @@ import FastImage from 'react-native-fast-image'
 //@refresh reset
 
 const {width,height} = Dimensions.get("screen")
-const minimizedHeight = height*.4;
+const minimizedHeight = height*.7;
 const EventHeaderImage = ( { imageUrl }: { imageUrl?: string } ) =>
 {
     const theme = useTheme()
-    const [visible, setVisible] = useState( true );
+    const [visible, setVisible] = useState( false );
 
      const [props, set] = useSpring( () => ( {
          opacity: 1, width: width, backgroundColor: Colors.background, maxHeight: minimizedHeight,
+         bm : "45%", h:minimizedHeight, top:"8%", blurImageHeight: "80%",
          blur : 6,
         config: {
             bounce: 100,
@@ -26,8 +27,11 @@ const EventHeaderImage = ( { imageUrl }: { imageUrl?: string } ) =>
     const onPress = () =>
     {
         set( {
-            maxHeight: visible? height : minimizedHeight,
-            blur : visible? 0 : 6
+            bm: visible? "0%" : "45%",
+            h: visible? height : minimizedHeight,
+            top: visible? "0%" : "8%",
+            blurImageHeight : visible? "100%" : "80%",
+            blur: visible? 10 : 6
         } ).then( () =>
         {
             setVisible(v => !v)
@@ -36,37 +40,39 @@ const EventHeaderImage = ( { imageUrl }: { imageUrl?: string } ) =>
 
     const TouchOp = animated( TouchableOpacity )
     const IMG = animated( FastImage )
+    const IMG1 = animated( Image )
+    const Vi = animated( View )
 
   
 
     return (
-       <View style={{marginBottom:"45%"}}>
-             <View center style={{width:"100%", height:"100%", zIndex:16, position:"absolute", top: "30%"}}>
-               <TouchableOpacity center activeOpacity={.9} onPress={onPress} style={{width:"95%", height:"130%", padding:20}} >
-               <View style={{elevation:10, width:"95%", height:"100%"}}>
+       <Vi style={{height:props.h, overflow:"visible", width, paddingBottom:20}}>
+                      <IMG1 
+            blurRadius={8}
+                source={{
+                    uri: imageUrl
+                }}
+                resizeMode="cover"
+                
+                style={{
+                    zIndex:1, backgroundColor:Colors.background,
+                height: props.blurImageHeight, width:"100%"
+                // top : -minimizedHeight
+                // top: props.maxHeight.to([minimizedHeight, height*.9], [-minimizedHeight,0])
+            }} />
+             <Vi  center style={{width:"100%", height:"100%", zIndex:16, position:"absolute", top: props.top, borderWidth:0, elevation:0}}>
+               <TouchableOpacity center activeOpacity={.9} onPress={onPress} style={{ borderWidth:0, elevation:0, width:"90%", height:"90%"}} >
                <IMG source={{
                     uri:imageUrl ,
                     priority : "normal"
                 }}
-                    style={{width:"100%", height:"100%"}}
-                    resizeMode={FastImage.resizeMode.contain}
+                    style={{width:"100%", height:"99%"}}
+                    resizeMode={ FastImage.resizeMode.contain}
                     />
-               </View>
                </TouchableOpacity>
-            </View>
-            <Image 
-            blurRadius={6.3}
-                source={{
-                    uri: imageUrl
-                }}
-                resizeMode={FastImage.resizeMode.cover}
-                
-                style={{
-                height: 390, width: "100%",
-                // top : -minimizedHeight
-                // top: props.maxHeight.to([minimizedHeight, height*.9], [-minimizedHeight,0])
-            }} />
-        </View>
+            </Vi>
+ 
+        </Vi>
     )
 }
 
--- pages/EventView.tsx
+++ pages/EventView.tsx
@@ -1,175 +1,159 @@
-import React, { useEffect, useRef, useState } from 'react';
-import
-    {
-        View,
-        Text,
-        Image,
-        Avatar,
-        Colors,
-        TouchableOpacity,
-    } from 'react-native-ui-lib';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { useTheme } from 'styled-components';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  Avatar,
+  Colors,
+  TouchableOpacity,
+} from 'react-native-ui-lib';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {useTheme} from 'styled-components';
 import FireStore from '../data_layer/FireStore';
-import { Easing, ScrollView, StatusBar } from 'react-native';
+import {Dimensions, Easing, ScrollView, StatusBar} from 'react-native';
 import moment from 'moment';
-import { FeedItemModel, PartyType } from '../universal/Models';
+import {FeedItemModel, PartyType} from '../universal/Models';
 import tm from '../universal/UiManager';
-import { GS, GetIcon, GetPartytypeString } from '../universal/GS';
+import {GS, GetIcon, GetPartytypeString} from '../universal/GS';
 import Organizer from '../components/Organizer';
 import UseSmallMapView from './UseSmallMapView';
 import PartyTypeBadge from '../components/PartyTypeBadge';
 import RSVPButton from '../components/RSVPButton';
 import EventHeaderImage from '../components/EventHeaderImage';
 import LocationBlock from '../components/LocationBlock';
-import { animated, useSpring } from 'react-spring/native';
+import {animated, useSpring} from 'react-spring/native';
 import Share from 'react-native-share';
 import rnfb from 'rn-fetch-blob';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
-const EventView = () =>
-{
-    const [image, setImage] = useState<string>();
-    const [base64, setBase64] = useState<string>( '' );
-    const navigation = useNavigation();
-    const theme = useTheme();
-    const route = useRoute();
-    const reference = route.params?.reference;
-    const item: FeedItemModel = FireStore.data.get( reference );
+const {width, height} = Dimensions.get('screen');
 
-    const getBase64 = () =>
-    {
-        rnfb
-            .fetch( 'GET', item.imageUrl )
-            .then( ( res ) =>
-            {
-                setBase64( res.base64() );
-            } )
-            .catch( ( err ) => console.log( err ) );
-    };
+const EventView = () => {
+  const [image, setImage] = useState<string>();
+  const [base64, setBase64] = useState<string>('');
+  const navigation = useNavigation();
+  const theme = useTheme();
+  const route = useRoute();
+  const reference = route.params?.reference;
+  const item: FeedItemModel = FireStore.data.get(reference);
 
-    const onShare = async () =>
-    {
-        let shareImage = 'data:image/png;base64,' + base64;
+  const getBase64 = () => {
+    rnfb
+      .fetch('GET', item.imageUrl)
+      .then((res) => {
+        setBase64(res.base64());
+      })
+      .catch((err) => console.log(err));
+  };
 
-        const { title, description, date, duration, reference } = item;
+  const onShare = async () => {
+    let shareImage = 'data:image/png;base64,' + base64;
 
+    const {title, description, date, duration, reference} = item;
 
+    const actualDate = moment(date).format('MMM M, YYYY');
+    Share.open({
+      title: `${title}\n`,
+      message: `${description}\n http://myparty.com/${reference}`,
+      url: shareImage,
+      failOnCancel: true,
+    });
+  };
 
-        const actualDate = moment( date ).format( "MMM M, YYYY" );
-        Share.open( {
-            title: `${ title }\n`,
-            message: `${ description }\n http://myparty.com/${ reference }`,
-            url: shareImage,
-            failOnCancel: true
-        } ); 
-    };
+  useEffect(() => {
+    async function getImage() {
+      const d = await FireStore.retrieve.imageFromReference(
+        item.reference,
+        item.flyer,
+      );
+      item.imageUrl = d;
+      setImage(d);
+    }
 
-    useEffect( () =>
-    {
-        async function getImage()
-        {
-            const d = await FireStore.retrieve.imageFromReference(
-                item.reference,
-                item.flyer,
-            );
-            item.imageUrl = d;
-            setImage( d );
-        }
+    if (item) {
+      getImage();
+      getBase64();
+    }
+  }, []);
 
-        if ( item )
-        {
-            getImage();
-            getBase64();
-        }
-    }, [] );
-
-    if ( item )
-        return (
-            <ScrollView
-                contentContainerStyle={{
-                    backgroundColor: Colors.background,
-                    minHeight: '100%',
-                    paddingBottom: 40,
-                }}>
-                <StatusBar animated backgroundColor={'rgba(0,0,0,0)'} />
-                <EventHeaderImage imageUrl={image} />
-                <View
-                    style={
-                        {
-                            // transform: [{ translateY: sp.y }],
-                            // opacity:sp.opacity
-                        }
-                    }>
-                    <View centerH>
-                        <PartyTypeBadge type={item?.partyType} />
-                    </View>
-                    <View bg-background padding-20 style={{ marginTop: -10 }}>
-                        <View marginV-5 centerV row spread>
-                            <Text lvl1>{item.title}</Text>
-                            <View row spread center>
-                                <RSVPButton reference={item.reference} />
-                                <TouchableOpacity onPress={onShare} marginB-7 marginL-12>
-                                    <Icon name="share-alt" size={22} color={Colors.text1} />
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                        <View marginT-20 row>
-                            <GetIcon name="calendar" />
-                            <View marginL-10>
-                                <Text lvl3 text3>
-                                    Date
-                </Text>
-                                <Text regular>
-                                    {moment( new Date( item.date ) ).format( 'ddd - MMM DD, YYYY' )}
-                                </Text>
-                            </View>
-                        </View>
-                        <View marginT-20 row>
-                            <GetIcon name="clock" />
-                            <View marginL-10>
-                                <Text lvl3 text3>
-                                    Time
-                </Text>
-                                <Text lvl2>
-                                    {moment( new Date( item.start || '' ) ).format( 'hh:mm A' )} for{' '}
-                                    {item.duration} hrs
-                </Text>
-                            </View>
-                        </View>
-                        <View marginT-20 row>
-                            <GetIcon name="info" />
-                            <View marginL-10>
-                                <Text marginT-7 marginB-3 lvl3 text3>
-                                    Description
-                </Text>
-                                <Text lvl2>{item.description}</Text>
-                            </View>
-                        </View>
-
-                        <View marginT-20 row>
-                            <GetIcon name="map" />
-                            <View marginL-10>
-                                <Text marginT-7 marginB-3 lvl3 text3>
-                                    Location
-                </Text>
-                                <LocationBlock {...item.locationObject} styled={true} />
-                            </View>
-                        </View>
-                    </View>
-                    <View>
-                        <UseSmallMapView loc={item.location} />
-                    </View>
-
-                    <Organizer
-                        org
-                        name={item.person || ''}
-                        reference={item.reference || ''}
-                    />
+  if (item) {
+    return (
+      <ScrollView
+      contentContainerStyle={{paddingBottom: 50, backgroundColor:Colors.background}}
+      >
+        <StatusBar animated backgroundColor={'rgba(0,0,0,0)'} />
+        <EventHeaderImage imageUrl={image} />
+        <View>
+            <View centerH>
+              <PartyTypeBadge type={item?.partyType} />
+            </View>
+            <View bg-background padding-20 style={{marginTop: -10}}>
+              <View marginV-5 centerV row spread>
+                <Text lvl1>{item.title}</Text>
+                <View row spread center>
+                  <RSVPButton reference={item.reference} />
+                  <TouchableOpacity onPress={onShare} marginB-7 marginL-12>
+                    <Icon name="share-alt" size={22} color={Colors.text1} />
+                  </TouchableOpacity>
                 </View>
-            </ScrollView>
-        );
-    else return <View />;
+              </View>
+              <View marginT-20 row>
+                <GetIcon name="calendar" />
+                <View marginL-10>
+                  <Text lvl3 text3>
+                    Date
+                  </Text>
+                  <Text regular>
+                    {moment(new Date(item.date)).format('ddd - MMM DD, YYYY')}
+                  </Text>
+                </View>
+              </View>
+              <View marginT-20 row>
+                <GetIcon name="clock" />
+                <View marginL-10>
+                  <Text lvl3 text3>
+                    Time
+                  </Text>
+                  <Text lvl2>
+                    {moment(new Date(item.start || '')).format('hh:mm A')} for{' '}
+                    {item.duration} hrs
+                  </Text>
+                </View>
+              </View>
+              <View marginT-20 row>
+                <GetIcon name="info" />
+                <View marginL-10>
+                  <Text marginT-7 marginB-3 lvl3 text3>
+                    Description
+                  </Text>
+                  <Text lvl2>{item.description}</Text>
+                </View>
+              </View>
+
+              <View marginT-20 row>
+                <GetIcon name="map" />
+                <View marginL-10>
+                  <Text marginT-7 marginB-3 lvl3 text3>
+                    Location
+                  </Text>
+                  <LocationBlock {...item.locationObject} styled={true} />
+                </View>
+              </View>
+            </View>
+            <View>
+              <UseSmallMapView loc={item.location} />
+            </View>
+
+            <Organizer
+              org
+              name={item.person || ''}
+              reference={item.reference || ''}
+            />
+          </View>
+      </ScrollView>
+    );
+  }
+  else return <View />;
 };
 
 export default EventView;
